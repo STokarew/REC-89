bf73c64c977a0b7ad68088af9edd4dd650663815
сергей токарев <sseregatokarew@gmail.com>
2021-11-04 17:18:18 +0700

    Добавьте файлы проекта.

---
+++ REC-89/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace REC_89
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число");
+            var n1=Convert.ToSingle(Console.ReadLine().Replace('.', ','));
+            Console.WriteLine("Введите второе число");
+            var n2 = Convert.ToSingle(Console.ReadLine().Replace('.', ','));
+            Console.WriteLine("Введите третье число");
+            var n3 = Convert.ToSingle(Console.ReadLine().Replace('.', ','));
+            var maxn = Math.Max(Math.Max(n1, n2), n3);
+            Console.WriteLine($"Максимальное число:  {maxn}");
+            Console.ReadLine();
+
+        }
+    }
+}
